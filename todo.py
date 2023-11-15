0257329f92f07c7b2393dcf32c79ac9ded78e09d
LumbarLa01 <98558460+LumbarLa01@users.noreply.github.com>
2023-11-14 20:07:49 -0700

    Update todo.py
    update, still need to figure out how to run this off a VM from the vscode terminal. Learning!

--- todo.py
+++ todo.py
@@ -44,3 +44,24 @@ def index():
     form = TaskForm()
     selection = list(r.table('todos').run(g.rdb_conn))
     return render_template('index.html', form=form, tasks=selection)
+
+## Finalizing the form
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    form = TaskForm()
+        if form.validate_on_submit():
+        r.table('todos').insert({"name":form.label.data}).run(g.rdb_conn)
+        return redirect(url_for('index'))
+    selection = list(r.table('todos').run(g.rdb_conn))
+    return render_template('index.html', form = form, tasks = selection)
+
+
+## Add these to do list items manually
+conn = rethink.connect(db='todo')
+rethinkdb.table('todos').insert({'name':'sail to the moon'}).run(conn)
+{u'errors': 0, u'deleted': 0, u'generated_keys': [u'c5562325-c5a1-4a78-8232-c0de4f500aff'], u'unchanged': 0, u'skipped': 0, u'replaced': 0, u'inserted': 1}
+rethinkdb.table('todos').insert({'name':'jump in the ocean'}).run(conn)
+{u'errors': 0, u'deleted': 0, u'generated_keys': [u'0a3e3658-4513-48cb-bc68-5af247269ee4'], u'unchanged': 0, u'skipped': 0, u'replaced': 0, u'inserted': 1}
+rethinkdb.table('todos').insert({'name':'think of another todo'}).run(conn)
+{u'errors': 0, u'deleted': 0, u'generated_keys': [u'b154a036-3c3b-47f4-89ec-cb9f4eff5f5a'], u'unchanged': 0, u'skipped': 0, u'replaced': 0, u'inserted': 1}
